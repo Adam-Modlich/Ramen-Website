33e9b17a40edaa0a7ff17b5df904a97b593084e4
Adam <259355@student.pwr.edu.pl>
2021-08-13 15:34:53 +0200

    drinks added

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function myFunction(){
     var x = document.getElementById("slider_drinks");
+    var y = document.getElementById("drinks_back");
+
     if (x.style.visibility == "visible"){
         x.style.visibility = "hidden";
+        y.style.visibility = "visible";
     }
     else{
         x.style.visibility = "visible";
+        y.style.visibility = "hidden";
+
     }
 }
